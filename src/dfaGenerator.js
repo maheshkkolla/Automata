c94f346eadb2e826499a582f3882478daae59ae2
Mahesh Kumar Kolla <mahesh.mk.245@gamil.com>
2016-06-04 15:19:41 +0530

    Basic dfa generator

---
+++ src/dfaGenerator.js
@@ -0,0 +1,27 @@
+module.exports = {
+	generate: function(tuples) {
+		return new Dfa(tuples.states, tuples.alphabets, tuples.transitionFunction, tuples.initialState, tuples.finalStates);
+	}
+}
+
+var Dfa = function(states, alphabets, transitionTable, initialState, finalStates) {
+	this.states = states;
+	this.alphabets = alphabets;
+	this.transitionTable = transitionTable
+	this.initialState = initialState;
+	this.finalStates = finalStates;
+};
+
+Dfa.prototype = {
+	transitionFunction: function(state, alphabet) {
+		return(this.transitionTable[state][alphabet]);
+	},
+
+	isInLaguage: function(inputString) {
+		var self = this;
+		var lastState = String(inputString).split("").reduce(function(state, alphabet) {
+			return self.transitionFunction(state, alphabet);
+		}, self.initialState);	
+		return((this.finalStates.indexOf(lastState)) >= 0);
+	}
+}
---
+++ test/dfaGeneratorTest.js
@@ -0,0 +1,20 @@
+var assert = require("chai").assert;
+var dfaGenerator = require("../src/dfaGenerator");
+
+describe("DFA Generator", function() {
+	it("generates DFA that ends with 0", function() {
+		var tuples = {
+			states: ["q1", "q2"],
+			alphabets: ["1", "0"],
+			transitionFunction: { 
+				"q1": { 0: "q2", 1: "q1"},
+				"q2": { 0: "q2", 1: "q1"} 
+			},
+			initialState: "q1",
+			finalStates: ["q2"]
+		};
+		var dfa = dfaGenerator.generate(tuples);
+		assert.isTrue(dfa.isInLaguage(1110));
+		assert.isFalse(dfa.isInLaguage(111));
+	});
+});
