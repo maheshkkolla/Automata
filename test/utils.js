3a0f6747843b1c5478a6a5cfaef4d87cc43c7c75
Mahesh Kumar Kolla <mahesh.mk.245@gamil.com>
2016-06-08 14:31:29 +0530

    test for array containsAll util function

--- test/utils.js
+++ test/utils.js
@@ -1,6 +1,14 @@
 var expect = require("chai").expect;
 var u = require("./utils");
 describe("utils", function() {
+    it("checks whether all the elements in one array present in another array", function() {
+    	var array1 = [2,4,6,5];
+    	var array2 = [4,5];
+    	var array3 = [4, 2, 1];
+    	expect(array1.containsAll(array2)).to.be.true;
+    	expect(array1.containsAll(array3)).to.be.false;
+    })
+
 	it("gives partial function for Math min", function() {
 		var partialMin = Math.min.partial({2:10,3:15,4:6});
 		expect(partialMin(20,44,32,65)).to.be.equals(6);
