7581daaa81a7965c8780db7e4b16fb59ad654554
Mahesh Kumar Kolla <mahesh.mk.245@gamil.com>
2016-06-08 14:27:49 +0530

    Add nfa test that has strings of length multiple by 2 or three

--- test/FiniteAutomataGenerator.js
+++ test/FiniteAutomataGenerator.js
@@ -138,7 +138,7 @@ describe("Automata Generator", function() {
 				},
 				initialState: "q1",
 				finalStates: ["q4"]
-			}
+			};
 
 			var nfa = new DfaGenerator().generateNfa(tuples);
 			expect(nfa.hasString('01010')).to.be.true;
@@ -160,7 +160,7 @@ describe("Automata Generator", function() {
 				},
 				initialState: "q1",
 				finalStates: ["q3", "q5"]
-			}
+			};
 
 			var nfa = new DfaGenerator().generateNfa(tuples);
 			expect(nfa.hasString('')).to.be.true;
@@ -168,6 +168,39 @@ describe("Automata Generator", function() {
 			expect(nfa.hasString('01010')).to.be.false;
 			expect(nfa.hasString('1101')).to.be.false;
 			expect(nfa.hasString('0000011111')).to.be.true;
+		});
+
+		it("generates Nfa for languge that has strings length divisible by 2 or 3", function() {
+			var tuples = {
+				states: ["q1", "q2", "q3", "q4", "q5", "q6"],
+				alphabets: ["0", "1"],
+				transitionTable: {
+					"q1": { 0: ["q2", "q4"], 1: ["q2", "q4"] },
+					"q2": { 0: ["q3"], 1: ["q3"] },
+					"q3": { 0: ["q2"], 1: ["q2"] },
+					"q4": { 0: ["q5"], 1: ["q5"] },
+					"q5": { 0: ["q6"], 1: ["q6"] },
+					"q6": { 0: ["q4"], 1: ["q4"] } 
+				},
+				initialState: "q1",
+				finalStates: ["q1", "q3", "q6"]
+			};
+
+			var nfa = new DfaGenerator().generateNfa(tuples);
+			expect(nfa.hasString('')).to.be.true;
+			expect(nfa.hasString('11')).to.be.true;
+			expect(nfa.hasString('01')).to.be.true;
+			expect(nfa.hasString('10')).to.be.true;
+			expect(nfa.hasString('00')).to.be.true;
+			expect(nfa.hasString('111')).to.be.true;
+			expect(nfa.hasString('101')).to.be.true;
+			expect(nfa.hasString('10111010101111')).to.be.true;
+			expect(nfa.hasString('101110101011110')).to.be.true;
+			
+			expect(nfa.hasString('1')).to.be.false;
+			expect(nfa.hasString('10111')).to.be.false;
+			expect(nfa.hasString('1011101')).to.be.false;
+			expect(nfa.hasString('10111010101')).to.be.false;
 		});
 	});
 });
